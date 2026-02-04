3604af5f8c01bdd933bca2980a6d6cc4abacd848
alestuff404 <valerinanda@gmail.com>
2026-02-04 16:02:16 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000127234_NandaValeri/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000127234_NandaValeri.week01
+
+fun main() {
+    var name: String = "Nanda Slayy"
+    var score: Int = 80
+
+    // concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
