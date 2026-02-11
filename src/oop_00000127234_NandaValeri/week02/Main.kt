5d50b89a1b96fdf8919ffe8b4e6012639d8340c1
alestuff404 <valerinanda@gmail.com>
2026-02-11 15:58:27 +0700

    week02: (task 1) implement library input system

--- src/oop_00000127234_NandaValeri/week02/Main.kt
+++ src/oop_00000127234_NandaValeri/week02/Main.kt
@@ -6,7 +6,6 @@ fun main() {
     val scanner = Scanner(System.`in`)
 
     println("--- APLIKASI PMB UMN ---")
-
     println("Masukkan nama: ")
     val name = scanner.nextLine()
 
@@ -24,17 +23,38 @@ fun main() {
         if (type == 1) {
             print("Masukkan Jurusan: ")
             val major = scanner.nextLine()
-            // Bagian ini harus 4 parameter: name, nim, 0.0, major
             val s1 = Student(name, nim, 0.0, major)
             println("Status: Pendaftaran Selesai.")
             println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
         } else if (type == 2) {
-            // Bagian ini memanggil secondary constructor (2 parameter)
             val s2 = Student(name, nim)
             println("Status: Pendaftaran Selesai.")
             println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
         } else {
-            println("Pilihan ngawur, pendaftaran Selesai!")
+            println("Pilihan ngawur!")
         }
     }
+
+    println("\n--- SISTEM PERPUSTAKAAN ---")
+    print("Judul Buku: ")
+    val title = scanner.nextLine()
+
+    print("Nama Peminjam: ")
+    val borrowerName = scanner.nextLine()
+
+    print("Lama Pinjam (Hari): ")
+    var duration = scanner.nextInt()
+    scanner.nextLine()
+
+    if (duration < 0) {
+        duration = 1
+    }
+
+    val loanData = Loan(title, borrowerName, duration)
+
+    println("\n--- DETAIL PEMINJAMAN ---")
+    println("Judul: ${loanData.bookTitle}")
+    println("Peminjam: ${loanData.borrower}")
+    println("Durasi: ${loanData.loanDuration} hari")
+    println("Total Denda: Rp ${loanData.calculateFine()}")
 }
