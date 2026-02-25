897bcfcc17058562c295c86064cb100d8c94c07b
alestuff404 <valerinanda@gmail.com>
2026-02-25 14:58:33 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000127234_NandaValeri/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000127234_NandaValeri.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("Toyota", numberOfDoors = 4)
+    myCar.openTrunk() // memanggil method milik sendiri
+    myCar.honk() // memanggil method yang sudah di-override
+    myCar.accelerate() // memanggil gabungan method parent dan child
+}
