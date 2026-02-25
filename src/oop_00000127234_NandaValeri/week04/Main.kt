c05ce4fc1ca41db4b7bad99746f541d1f941d97f
alestuff404 <valerinanda@gmail.com>
2026-02-25 15:15:32 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_00000127234_NandaValeri/week04/Main.kt
+++ src/oop_00000127234_NandaValeri/week04/Main.kt
@@ -18,4 +18,15 @@ fun main() {
     tesla.accelerate()
     tesla.honk()
     tesla.openTrunk()
+
+    println("\n===== EMPLOYEE HIERARCHY TEST =====")
+
+    val manager = Manager("Budi", 10_000_000)
+    val developer = Developer("Nanda", 8_000_000, "Kotlin")
+
+    manager.work()
+    println("Bonus Manager: Rp ${manager.calculateBonus()}")
+
+    developer.work()
+    println("Bonus Developer: Rp ${developer.calculateBonus()}")
 }
