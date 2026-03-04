34a0cad5fd740d5b3811006098915e1fa017a0d5
alestuff404 <valerinanda@gmail.com>
2026-03-04 15:11:09 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000127234_NandaValeri/week05/EWallet.kt
@@ -0,0 +1,23 @@
+package oop_00000127234_NandaValeri.week05
+
+class EWallet(
+    accountName: String,
+    var balance: Double
+) : PaymentMethod(accountName) {
+
+    override fun processPayment(amount: Double) {
+        println("Memproses pembayaran E-Wallet oleh $accountName sebesar $amount")
+
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran berhasil. Sisa saldo: $balance")
+        } else {
+            println("Saldo tidak cukup.")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top up berhasil. Saldo sekarang: $balance")
+    }
+}
