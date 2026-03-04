3b5619a1e7b006b8ee22c1e5088b243e63f42c87
alestuff404 <valerinanda@gmail.com>
2026-03-04 15:08:01 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000127234_NandaValeri/week05/Main.kt
+++ src/oop_00000127234_NandaValeri/week05/Main.kt
@@ -1,8 +1,12 @@
 package oop_00000127234_NandaValeri.week05
 
 fun main() {
+
+    // ===============================
+    // POLYMORPHISM (OVERRIDING)
+    // ===============================
     val dosen1 = Dosen("Pak Alex", "0123456")
-    val admin1 =Admin("Bu Siti")
+    val admin1 = Admin("Bu Siti")
 
     val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
 
@@ -12,7 +16,7 @@ fun main() {
 
         when (pegawai) {
             is Dosen -> {
-                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn}")
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
                 pegawai.mengajar()
             }
             is Admin -> {
@@ -22,4 +26,20 @@ fun main() {
         }
         println("-------------------------------")
     }
+
+    // ===============================
+    // POLYMORPHISM (OVERLOADING)
+    // ===============================
+    println("\n=== PERHITUNGAN LUAS BANGUN DATAR ===")
+
+    val mathHelper = MathHelper()
+
+    val luasPersegi = mathHelper.hitungLuas(4)
+    println("Luas Persegi (sisi = 4): $luasPersegi")
+
+    val luasPersegiPanjang = mathHelper.hitungLuas(5, 3)
+    println("Luas Persegi Panjang (5 x 3): $luasPersegiPanjang")
+
+    val luasLingkaran = mathHelper.hitungLuas(7.0)
+    println("Luas Lingkaran (r = 7.0): $luasLingkaran")
 }
