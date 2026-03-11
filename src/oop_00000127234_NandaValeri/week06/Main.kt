780a66206f48842f6cd304c0690a77d7ca5b0e28
alestuff404 <valerinanda@gmail.com>
2026-03-11 16:22:40 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_00000127234_NandaValeri/week06/Main.kt
+++ src/oop_00000127234_NandaValeri/week06/Main.kt
@@ -6,16 +6,32 @@ fun processCheckout(method: PaymentMethod, amount: Double) {
 }
 
 fun main() {
+
+    // ===============================
+    // TEST DEVICE DASAR
+    // ===============================
     val myWatch = SmartWatch()
     myWatch.showTime()
 
     val myPhone = Smartphone()
     myPhone.turnOn()
 
+    // ===============================
+    // TEST PAYMENT SYSTEM
+    // ===============================
     val pay1 = Gopay()
     val pay2 = CreditCard()
 
     println("\n=== TESTING CHECKOUT ===")
     processCheckout(pay1, 50000.0)
     processCheckout(pay2, 150000.0)
+
+    // ===============================
+    // SMART HOME SYSTEM (Checkpoint 19 & 20)
+    // ===============================
+    println("\n=== SMART HOME SYSTEM ===")
+
+    val lamp = SmartLamp("L001", "Ruang Tamu")
+    val speaker = SmartSpeaker("S001", "Google Nest Dapur")
+    val cctv = SmartCCTV("C001", "Ezviz Garasi")
 }
