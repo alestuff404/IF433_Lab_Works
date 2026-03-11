b65ea70d4771f7d9a7a5a39410314c14254536c8
alestuff404 <valerinanda@gmail.com>
2026-03-11 15:20:52 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000127234_NandaValeri/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000127234_NandaValeri.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
