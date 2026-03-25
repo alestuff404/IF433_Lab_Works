d0c6ba568709e806ef9eff84d3f4f5b869c065d0
alestuff404 <valerinanda@gmail.com>
2026-03-25 16:34:47 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000127234_NandaValeri/week07/SystemStates.kt
+++ src/oop_00000127234_NandaValeri/week07/SystemStates.kt
@@ -1,5 +1,13 @@
 package oop_00000127234_NandaValeri.week07
 
+import javax.xml.crypto.Data
+
 enum class AppState {
     STARTING, RUNNING, STOPPED
+}
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val massage: String) : ApiResponse()
+    object Loading: ApiResponse()
 }
