6b42bd3181c31ecf4773c00b762bda621ada06cf
alestuff404 <valerinanda@gmail.com>
2026-04-15 15:44:01 +0700

    week08: (task) create Java Payment Service for payment interop

---
+++ src/oop_00000127234_NandaValeri/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000127234_NandaValeri.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
