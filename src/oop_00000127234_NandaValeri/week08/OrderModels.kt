27f850d8de658d0a59a56730411b17efd6baca82
alestuff404 <valerinanda@gmail.com>
2026-04-15 15:06:13 +0700

    week08: implement safe call with let block

--- src/oop_00000127234_NandaValeri/week08/OrderModels.kt
+++ src/oop_00000127234_NandaValeri/week08/OrderModels.kt
@@ -3,4 +3,4 @@ package oop_00000127234_NandaValeri.week08
 class City(val name:String)
 class Address(val city:City?)
 class DeliveryDetails(val address:Address?)
-class Order(val delivery:DeliveryDetails?, val totalPrice:Int?)
+class Order(val deliveryDetails:DeliveryDetails?, val totalPrice:Int?)
