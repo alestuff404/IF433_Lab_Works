161394f7d321bb46286182dd78d3cc0964f24280
alestuff404 <valerinanda@gmail.com>
2026-05-06 14:53:49 +0700

    week11: create string extension functions

---
+++ src/oop_00000127234_NandaValeri/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000127234_NandaValeri.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
