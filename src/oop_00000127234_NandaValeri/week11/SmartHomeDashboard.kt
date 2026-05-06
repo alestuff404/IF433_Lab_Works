eff3ae5c2909ca1617eb1ad524501bf620009b92
alestuff404 <valerinanda@gmail.com>
2026-05-06 15:14:20 +0700

    week11: (task) test full smart home configuration pipeline

--- src/oop_00000127234_NandaValeri/week11/SmartHomeDashboard.kt
+++ src/oop_00000127234_NandaValeri/week11/SmartHomeDashboard.kt
@@ -35,5 +35,8 @@ fun main() {
     val totalPower = homeDevices.run { sumOf { it.powerLoad } }
     println("Total penggunaan daya: $totalPower Watt")
 
-
+    println("=== STATUS PERANGKAT ===")
+    homeDevices.forEach {
+        println(it.diagnose())
+    }
 }
