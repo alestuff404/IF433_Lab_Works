3d7bd040f7cf068c72ebd28f1f9b0c83cb7eb36a
alestuff404 <valerinanda@gmail.com>
2026-05-06 15:08:03 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000127234_NandaValeri/week10/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000127234_NandaValeri.week10
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
